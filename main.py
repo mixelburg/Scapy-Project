1ce73ba4b7d4071fa3220ae449e21a87e30db9b8
mixelburg <52622705+mixelburg@users.noreply.github.com>
2020-06-15 01:38:48 +0300

    Major commit

--- main.py
+++ main.py
@@ -1,43 +1,54 @@
 # check installed packages
-from scapy.layers.inet import IP, UDP
-from scapy.packet import Raw
-from scapy.sendrecv import sr1, sniff, send, srp1
-
 import myLib
-
 myLib.install_check()
 
 from wrappers import make_green, make_red, make_blue
 import scapy
+from scapy.layers.inet import IP, UDP
+from scapy.packet import Raw
+from scapy.sendrecv import sr1, sniff, send, srp1
+import hashlib
 
 server_IP = "54.71.128.194"
 sever_Port = 99
 bufferSize = 1024
 
+vehicle_id = ''
+airport = ''
+sent = False
+
+DESTINATION = "jupiter"
+
 MESSAGE_WAKE = "WAK000up"
 MESSAGE_ENT = "ENT"
 MESSAGE_ERROR = "ERR"
 MESSAGE_DATA = ""
+MESSAGE_FLY = "FLY"
+MESSAGE_LANDING = "location_md5={},airport={},time=15:52,lane=earth.jup,vehicle={},fly"
 
 
 def get_char(char, iterations):
-    value = ord(char)
+    if char.isalpha():
+        value = ord(char)
 
-    new_value = value + iterations
-    if new_value > 122:
-        new_value = new_value % 122 + 96
+        new_value = value + iterations
+        if new_value > 122:
+            new_value = new_value % 122 + 96
 
-    return chr(new_value)
+        return chr(new_value)
+    return char
 
 
 def get_reverse_char(char, iterations):
-    value = ord(char)
+    if char.isalpha():
+        value = ord(char)
 
-    new_value = value - iterations
-    if new_value < 97:
-        new_value = new_value + 26
+        new_value = value - iterations
+        if new_value < 97:
+            new_value = new_value + 26
 
-    return chr(new_value)
+        return chr(new_value)
+    return char
 
 
 def encrypt(data, iterations):
@@ -89,18 +100,48 @@ def alien_checker(packet):
 def alien_print(packet):
     server_answer = packet[Raw].load.decode()
     status, code, message = answer_separator(server_answer)
+    message = decrypt(message, code)
+
+    global vehicle_id
+    global airport
+    global sent
 
     print(f"Status: {status}, code: {code}")
-    print(f"Decrypted: {decrypt(message, code)}")
+    print(f"Decrypted: {message}")
+
+    if "vehicle chosed" in message:
+        vehicle_id = message.split(' id ')[1]
+    elif "airport selected" in message:
+        airport = message.split("takeoff: ")[1]
+
+    if not sent and vehicle_id != '' and airport != '':
+        send_fly(vehicle_id, airport)
+        sent = True
+
+
+def send_fly(vehicle, airprt):
+    # sending landing info
+    print("Giving md5")
+    str2hash = DESTINATION
+    result = hashlib.md5(str2hash.encode())
+    result = result.hexdiget()
+
+    data = MESSAGE_LANDING.format(result, airprt, vehicle)
+    print("********************************")
+    print(f"Data: {data}")
+    payload = encrypt(data, 8)
+
+    udp_message = IP(dst=server_IP) / UDP(dport=sever_Port) / Raw(load=MESSAGE_FLY + "008" + payload)
+    send(udp_message)
 
 
 def main():
+    # sending destination planet
     print("Sending Aliens to Jupiter")
-    payload = encrypt("jupiter", 3)
-    print(payload)
+    payload = encrypt(DESTINATION, 3)
     udp_message = IP(dst=server_IP) / UDP(dport=sever_Port) / Raw(load=MESSAGE_ENT + "003" + payload)
-
     send(udp_message)
+
     print("Sniffing answers: ")
     packets = sniff(lfilter=alien_checker, prn=alien_print)
 
