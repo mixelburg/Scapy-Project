e0ea534f156069c00169c2b4859e1a3ac41fbfd2
mixelburg <52622705+mixelburg@users.noreply.github.com>
2020-06-18 11:25:37 +0300

    Very Final Commit

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from scapy.packet import Raw
 from scapy.sendrecv import sr1, sniff, send, srp1, sendp
 import hashlib
 from colorama import init, Fore
+from datetime import datetime
 
 # initialize colorama
 init()
@@ -19,6 +20,11 @@ GREEN = Fore.GREEN
 BLUE = Fore.BLUE
 RESET = Fore.RESET
 MAGENTA = Fore.MAGENTA
+
+# ASCII info
+FIRST_ASCII = 97
+LAST_ASCII = 122
+NUM_ASCII = 26
 
 # server info
 server_IP = "54.71.128.194"
@@ -32,14 +38,19 @@ airport = ''
 sent = False
 sent_planet = False
 location_data = {}
+NUM_LOCATION_PARTS = 10
 DESTINATION = "jupiter"
 
 # alien protocol data
 MESSAGE_ENT = "ENT"
 MESSAGE_ERROR = "ERR"
 MESSAGE_FLY = "FLY"
+MESSAGE_END = "YES"
 # landing data pattern
 MESSAGE_LANDING = "location_md5={},airport={},time=15:52,lane=earth.jup,vehicle={},fly"
+
+# time
+DATETIME_FORMAT = '%B %d, %Y | %H:%M:%S'
 
 
 def print_end():
@@ -62,103 +73,70 @@ def print_start():
     prints program info
     :return: None
     """
-    print(f"""{BLUE}
-     _   _                      _            _   _             
-    | | | |                    | |          | | (_)            
-    | |_| |__   ___   ___  __ _| |_   ____ _| |_ _  ___  _ __  
-    | __| '_ \ / _ \ / __|/ _` | \ \ / / _` | __| |/ _ \| '_ \\
-    | |_| | | |  __/ \__ \ (_| | |\ V / (_| | |_| | (_) | | | |
-     \__|_| |_|\___| |___/\__,_|_| \_/ \__,_|\__|_|\___/|_| |_|
+    print(f"""{GREEN}
+     _   _                         _            _   _             
+    | | | |                       | |          | | (_)            
+    | |_| |__   ___      ___  __ _| |_   ____ _| |_ _  ___  _ __  
+    | __| '_ \ / _ \    / __|/ _` | \ \ / / _` | __| |/ _ \| '_ \\
+    | |_| | | |  __/    \__ \ (_| | |\ V / (_| | |_| | (_) | | | |
+     \__|_| |_|\___|    |___/\__,_|_| \_/ \__,_|\__|_|\___/|_| |_|
     created by: mixelburg 
     {RESET}""")
 
 
-def get_char(char, iterations):
+def get_char(char, iterations, reverse=False):
     """
-    encrypts given char according to given encryption data
+    encrypts or decrypts given char according to
+    given encryption data and 'reverse' flag
     :param char: letter
     :param iterations: encryption data
+    :param reverse: flag
     :return: encrypted char
     """
     if char.isalpha():
         # get ascii value
         value = ord(char)
 
-        # get new ascii value
-        new_value = value + iterations
-        if new_value > 122:
-            new_value = new_value % 122 + 96
+        if reverse:
+            # get new ascii value
+            new_value = value - iterations
+            if new_value < FIRST_ASCII:
+                new_value = new_value + NUM_ASCII
+        else:
+            # get new ascii value
+            new_value = value + iterations
+            if new_value > LAST_ASCII:
+                new_value = new_value % LAST_ASCII + (FIRST_ASCII - 1)
 
         return chr(new_value)
     return char
 
 
-def get_reverse_char(char, iterations):
+def encrypt_decrypt(data, iterations, reverse=False):
     """
-    decrypts data according to given encryption data
-    :param char: encrypted letter
-    :param iterations: encryption data
-    :return: decrypted letter
-    """
-    if char.isalpha():
-        # get ascii value
-        value = ord(char)
-
-        # get new ascii value
-        new_value = value - iterations
-        if new_value < 97:
-            new_value = new_value + 26
-
-        return chr(new_value)
-    return char
-
-
-def encrypt(data, iterations):
-    """
-    encrypts give data (text) according to a given encryption data
-    :param data: text
-    :param iterations: encryption data
-    :return: encrypted data
+    decrypts or encrypts data according to 'reverse' flag
+    :param data: data to work with
+    :param iterations: encryption pattern
+    :param reverse: flag
+    :return: reworked data
     """
     data = data.lower()
     result = ""
 
-    # encrypt data
-    for i in range(len(data)):
-        # pass every second letter
+    # encrypt or decrypt data
+    for i in range(len(data)):  # for letter in string
+        # skip every second letter
         if (i + 1) % 2 == 0:
             result += data[i]
-        # pass spaces
+        # skip spaces
         elif data[i] == ' ':
             result += ' '
         else:
-            # get decrypted char
-            result += get_char(data[i], iterations)
-
-    return result
-
-
-def decrypt(data, iterations):
-    """
-    decrypts give data (text) according to a given decryption data
-    :param data: text
-    :param iterations: encryption data
-    :return: decrypted data
-    """
-    data = data.lower()
-    result = ""
-
-    # decrypt data
-    for i in range(len(data)):
-        # pass every second letter
-        if (i + 1) % 2 == 0:
-            result += data[i]
-        # pass spaces
-        elif data[i] == ' ':
-            result += ' '
-        else:
-            # get decrypted char
-            result += get_reverse_char(data[i], iterations)
+            # get decrypted or encrypted char according to 'reverse' flag
+            if reverse:
+                result += get_char(data[i], iterations, True)
+            else:
+                result += get_char(data[i], iterations)
 
     return result
 
@@ -170,7 +148,11 @@ def answer_separator(server_answer):
     :return: separated data
     """
     status = server_answer[:3]
-    code = int(server_answer[3:6])
+    try:
+        code = int(server_answer[3:6])
+    except ValueError:
+        print("Invalid code, setting it to 0 (no encryption)")
+        code = 0
     message = server_answer[6:]
 
     return status, code, message
@@ -194,19 +176,21 @@ def alien_print(packet):
     :param packet: sniffed packet
     :return: None
     """
-    # getting data and decrypting it
-    server_answer = packet[Raw].load.decode()
-    status, code, message = answer_separator(server_answer)
-    message = decrypt(message, code)
-
-    # getting port of Alien Client
     global local_port
-    local_port = packet[UDP].dport
-
     global vehicle_id
     global airport
     global sent
     global sent_planet
+
+    # getting data and decrypting it
+    server_answer = packet[Raw].load.decode()
+    # send data to a separator
+    status, code, message = answer_separator(server_answer)
+    # decrypt data
+    message = encrypt_decrypt(message, code, reverse=True)
+
+    # getting port of Alien Client
+    local_port = packet[UDP].dport
 
     # printing server answer data
     print(f"""{GREEN}
@@ -220,12 +204,13 @@ def alien_print(packet):
         print(f"{RED}Sending Aliens to Jupiter{RESET}")
 
         # encrypting data
-        payload = encrypt(DESTINATION, 3)
+        payload = encrypt_decrypt(DESTINATION, 3)
 
-        # sending
+        # creating message and sending it
         udp_message = IP(dst=server_IP) / UDP(dport=sever_Port, sport=local_port) / Raw(
             load=MESSAGE_ENT + "003" + payload)
         send(udp_message)
+        # raising the flag
         sent_planet = True
     elif "vehicle chosed" in message:
         # collecting vehicle info
@@ -247,20 +232,27 @@ def alien_print(packet):
         {RESET}""")
     elif "location data" in message:
         # collecting airport info and parcing it
+
+        # split location data
         data = message[13:]
+        # get num of location data
         number = data.split("/")[0]
+        # get the status code
         info = data.split(": ")[1]
+        # store part of location data (use dictionary to avoid repeating)
         location_data[number] = info
 
         # printing location data
         print(f"""{RED} 
         Data: {data} 
         {RESET}""")
-    elif status == "YES":
+    # if aliens agreed to go home, then print the end message
+    elif status == MESSAGE_END:
         print_end()
+        raise KeyboardInterrupt
 
     # checking if it is possible to send them back
-    if not sent and vehicle_id != '' and airport != '' and len(location_data) == 10:
+    if not sent and vehicle_id != '' and airport != '' and len(location_data) == NUM_LOCATION_PARTS:
         data = ""
         # creating location data string
         for val in location_data.values():
@@ -268,6 +260,7 @@ def alien_print(packet):
 
         # sending aliens home
         send_fly(vehicle_id, airport, data)
+        # raise the flag
         sent = True
 
 
@@ -288,7 +281,7 @@ def send_fly(vehicle, airprt, location):
     # printing data
     print(f"{BLUE}Sending a FLY request: {RESET}")
     # creating payload
-    payload = encrypt(data, 8)
+    payload = encrypt_decrypt(data, 8)
 
     # sending
     udp_message = IP(dst=server_IP) / UDP(dport=sever_Port, sport=local_port) / Raw(load=MESSAGE_FLY + "008" + payload)
@@ -296,11 +289,14 @@ def send_fly(vehicle, airprt, location):
 
 
 def main():
+    # print program info
     print_start()
     print(f"{BLUE}Collecting data: {RESET}")
+    print(f"{BLUE}Please open Alien client{RESET}")
 
     # starting data collection
     packets = sniff(lfilter=alien_checker, prn=alien_print)
+    print(f"Earth saved on: {datetime.now().strftime(DATETIME_FORMAT)}")
 
 
 if __name__ == '__main__':
